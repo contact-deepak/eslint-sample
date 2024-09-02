32ef0f7df6fcaea3bdc4f2f0cd642f4f76732a76
contact-deepak <contact.deepakb@gmail.com>
2024-09-02 17:05:14 +0530

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    ignores: ["node_modules/**"],
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+    },
+    rules: {
+      "semi": ["error", "always"],
+      "quotes": ["error", "single"],
+    },
+  },
+];
